b0e24953c000471ba9f8020a0de3e81d9ee0ee98
Hue  Vang <huevang@Hues-MacBook-Pro.local>
2017-02-03 11:12:07 -0600

    Added a ''while loop'' insideof the ''search'' function, which enables it to pick the next listed gif under the same search name. Made the service send its array to the controller, instead of sending back a URL result.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -8,28 +8,36 @@ app.controller('GiphyController', function(gifService){
 
   ctrl.giphyList = [];
 
-  // ctrl.random = function() {
-  //   var test = gifService.getRandom();
-  //   ctrl.giphyList = gifService.getRandom();
-  //   console.log('This is ctrl giphyList', test.$$state.value);
-  // }; // end random
-  //
-  // ctrl.giphySearch = function() {
-  //   ctrl.giphyList = gifService.gifSearch();
-  //   console.log('This is ctrl giphyList', ctrl.giphyList);
-  // }; // end giphySearch
+  // Retrieves the array from the service with a new random gif.
+  ctrl.random = function() {
+    gifService.getRandom().then(function(newGiphyList) {
+      ctrl.giphyList = newGiphyList;
+    })
+  }; // end random
 
-ctrl.random = function() {
-  gifService.getRandom().then(function(randomGif) {
-    ctrl.giphyList.push(randomGif);
-  }); // end getRandom
-}; // end random
+  // Retrieves the array from the service with a new searched gif.
+  ctrl.giphySearch = function(gifName) {
+    gifService.gifSearch(gifName).then(function(newGiphyList) {
+      ctrl.giphyList = newGiphyList;
+    });
+    console.log('This is client.js giphyList', ctrl.giphyList);
+  }; // end giphySearch
 
-ctrl.giphySearch = function(searchedGif) {
-  gifService.gifSearch(searchedGif).then(function(searchedGif) {
-    ctrl.giphyList.push(searchedGif);
-    console.log('This is giphy list', ctrl.giphyList);
-  }); // end giphySearch
-}; // end giphySearch
+
+
+
+// Working Code
+// ctrl.random = function() {
+//   gifService.getRandom().then(function(randomGif) {
+//     ctrl.giphyList.push(randomGif);
+//   }); // end getRandom
+// }; // end random
+//
+// ctrl.giphySearch = function(searchedGif) {
+//   gifService.gifSearch(searchedGif).then(function(searchedGif) {
+//     ctrl.giphyList.push(searchedGif);
+//     console.log('This is giphy list', ctrl.giphyList);
+//   }); // end giphySearch
+// }; // end giphySearch
 
 });
--- public/scripts/gif-service.js
+++ public/scripts/gif-service.js
@@ -2,14 +2,44 @@ app.service('gifService', function($http) {
 
 var RANDOMAPI = 'http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC'
 
-// var giphyList = [];
+var giphyList = [];
 
+// gets a random gif URL, adds its to the giphyList array, and returns the array to the controller.
+this.getRandom = function() {
+  return $http.get(RANDOMAPI).then(function(response){
+    console.log('Gif service', response);
+    giphyList.push(response.data.data.image_original_url);
+    // return response.data.data.image_original_url;
+    return giphyList;
+  }).catch(function(err){
+    console.log('Error', err);
+  });
+} // end getRandom function
+
+// gets a searced gif URL if not already on the array, adds its to the giphyList array, and returns the array to the controller.
+this.gifSearch = function(gifName) {
+  var searchGif = ('http://api.giphy.com/v1/gifs/search?q=' + gifName + '&api_key=dc6zaTOxFJmzC');
+    return $http.get(searchGif).then(function(response){
+    console.log('Gif', response);
+    var index = 0;
+    while (giphyList.includes(response.data.data[index].images.original.url)) {
+      index += 1;
+    }
+    giphyList.push(response.data.data[index].images.original.url);
+    console.log('This is gif-service.js giphyList', giphyList);
+    // return response.data.data[index].images.original.url;
+    return giphyList;
+  }).catch(function(err){
+    console.log('Error', err);
+  })
+} // end giphySearch function
+
+
+// Working CODE
 // this.getRandom = function() {
 //   return $http.get(RANDOMAPI).then(function(response){
 //     console.log('Gif ', response);
-//     giphyList.push(response.data.data.image_original_url);
-//     // return response.data.data.image_original_url;
-//     return giphyList;
+//     return response.data.data.image_original_url;
 //   }).catch(function(err){
 //     console.log('Error', err);
 //   });
@@ -19,36 +49,10 @@ var RANDOMAPI = 'http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC'
 //   var searchGif = ('http://api.giphy.com/v1/gifs/search?q=' + gifName + '&api_key=dc6zaTOxFJmzC');
 //   return $http.get(searchGif).then(function(response){
 //     console.log('Gif', response);
-//     var index = 0;
-//     while (giphyList.includes(response.data.data[index].images.original.url)) {
-//       index += 1;
-//     }
-//     giphyList.push(response.data.data[index].images.original.url);
-//     console.log('This is giphy list', giphyList);
-//     // return response.data.data[index].images.original.url;
-//     return giphyList;
+//     return response.data.data[0].images.original.url;
 //   }).catch(function(err){
 //     console.log('Error', err);
 //   })
 // } // end giphySearch function
-
-this.getRandom = function() {
-  return $http.get(RANDOMAPI).then(function(response){
-    console.log('Gif ', response);
-    return response.data.data.image_original_url;
-  }).catch(function(err){
-    console.log('Error', err);
-  });
-} // end getRandom function
-
-this.gifSearch = function(gifName) {
-  var searchGif = ('http://api.giphy.com/v1/gifs/search?q=' + gifName + '&api_key=dc6zaTOxFJmzC');
-  return $http.get(searchGif).then(function(response){
-    console.log('Gif', response);
-    return response.data.data[0].images.original.url;
-  }).catch(function(err){
-    console.log('Error', err);
-  })
-} // end giphySearch function
 
 }) // end app.service
